087d6fc3dad0b22e8a3120ea38ea7d10d8e64c33
Travis Lloyd <travis.t.lloyd1996@gmail.com>
2021-09-29 22:16:03 -0600

    added error proofing for form input

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,16 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
     //creates data object to be made a task
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
     };
-    
+    formEl.reset();
     // send it as an argument to createTaskEl
     createTaskEl(taskDataObj);
 
